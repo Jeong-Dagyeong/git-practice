c413c24c234588d4500e0fb10bd09f1c6da5a8a0
dagyeong Jeong <jdk09140@gmail.com>
2022-12-28 19:27:32 +0900

    ADD : git feature/login 브랜치 생성, Login.js / Login.css 폴더 생성

---
+++ Frontend/git-test/src/pages/Login/Login.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Login.css";
+
+export default function Login() {
+  const [userInfo, setUserInfo] = useState({ id: "", password: "" });
+  const navigate = useNavigate();
+
+  const getUserInfo = (e) => {
+    const { name, value } = e.target;
+    setUserInfo({ ...userInfo, [name]: value });
+  };
+
+  const isValid = userInfo.id.includes("@") && userInfo.password.length >= 5;
+
+  const goToMain = (e) => {
+    e.preventDefault();
+    navigate("/main");
+  };
+
+  return (
+    <div className="login">
+      <main className="loginContainer">
+        <h1 className="title">Git Test</h1>
+        <form className="loginForm" onSubmit={goToMain}>
+          <input
+            className="userId"
+            name="id"
+            type="text"
+            value={userInfo.id}
+            placeholder="이메일을 입력해 주세요."
+            onChange={getUserInfo}
+          />
+          <input
+            className="userPassword"
+            name="password"
+            type="password"
+            value={userInfo.password}
+            placeholder="비밀번호를 입력해 주세요."
+            onChange={getUserInfo}
+          />
+          <button className="loginButton" type="submit" disabled={!isValid}>
+            로그인
+          </button>
+        </form>
+      </main>
+    </div>
+  );
+}
